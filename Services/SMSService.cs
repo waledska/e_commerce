39673a5e6a62cf97972e3b0635bf1927f13fc50b
alizy <alizy@SAKSOOOK>
2024-03-18 16:48:54 +0200

    makeItAgainnotComment

--- Services/SMSService.cs
+++ Services/SMSService.cs
@@ -1,30 +1,30 @@
-﻿//using e_commerce.Helpers;
-//using Microsoft.Extensions.Options;
-//using Twilio;
-//using Twilio.Rest.Api.V2010.Account;
+﻿using e_commerce.Helpers;
+using Microsoft.Extensions.Options;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
-//namespace e_commerce.Services
-//{
-//    public class SMSService: ISMSService
-//    {
-//        private readonly TwilioSettings _twilio;
+namespace e_commerce.Services
+{
+    public class SMSService: ISMSService
+    {
+        private readonly TwilioSettings _twilio;
 
-//        public SMSService(IOptions<TwilioSettings> twilio)
-//        {
-//            _twilio = twilio.Value;
-//        }
+        public SMSService(IOptions<TwilioSettings> twilio)
+        {
+            _twilio = twilio.Value;
+        }
 
-//        public MessageResource Send(string mobileNumber, string body)
-//        {
-//            TwilioClient.Init(_twilio.AccountSID, _twilio.AuthToken);
+        public MessageResource Send(string mobileNumber, string body)
+        {
+            TwilioClient.Init(_twilio.AccountSID, _twilio.AuthToken);
 
-//            var result = MessageResource.Create(
-//                    body: body,
-//                    from: new Twilio.Types.PhoneNumber(_twilio.TwilioPhoneNumber),
-//                    to: mobileNumber
-//                );
+            var result = MessageResource.Create(
+                    body: body,
+                    from: new Twilio.Types.PhoneNumber(_twilio.TwilioPhoneNumber),
+                    to: mobileNumber
+                );
 
-//            return result;
-//        }
-//    }
-//}
+            return result;
+        }
+    }
+}
